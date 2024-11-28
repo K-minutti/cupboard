cb4dc87831d465ee186c6f51f02078a2818d8862
Kevin Minutti <minutti70@gmail.com>
2024-11-28 12:53:44 -0500

    Simple output of keystrokes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cupboard
+
+A suite of things you can do during down time right in your terminal
--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
 
 import (
-    "fmt"
-    "flag"
+	"fmt"
+	"syscall"
 )
 
 func main() {
-    var input string
-    flag.StringVar(&input, "i", "", "input name")
-    flag.Parse()
-    
-    output := fmt.Sprintf("Hello %s :)", input)
-    fmt.Println(output)
+
+    fmt.Println("Starting program...")
+
+    for {
+        key, err := readKeyStroke()
+        if err != nil {
+            fmt.Println("Error reading keystroke:", err)
+            return
+        }
+        fmt.Printf("%c", key)
+    }
+}
+
+func readKeyStroke() (byte, error) {
+    var buf [1]byte
+    _, err := syscall.Read(syscall.Stdin, buf[:])
+    if err != nil {
+        return 0, err
+    }
+    return buf[0], nil
+
 }
