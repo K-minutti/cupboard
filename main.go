91465fa1c7e32f8375f9c5e2cdd767a939faf6d0
Kevin Minutti <minutti70@gmail.com>
2024-11-30 13:35:01 -0500

    Add text output to type against

--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
+    "time"
+    "math/rand"
 	"syscall"
 )
 
-func main() {
+var data = []string{
+    "The only thing we have to fear is fear itself",
+    "Ask not what your country can do for you; ask what you can do for your country",
+    "Tis better to have loved and lost than never to have loved at all",
+}
 
+
+func main() {
     fmt.Println("Starting program...")
 
+    rand.Seed(time.Now().UnixNano())
+    randIdx := rand.Intn(len(data))
+
+    text := data[randIdx]
+
+    fmt.Println(text)
+    
     for {
         key, err := readKeyStroke()
         if err != nil {
