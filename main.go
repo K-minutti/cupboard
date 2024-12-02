624131a345ca22317de0d328b7c46ad465ba9bc1
Kevin Minutti <minutti70@gmail.com>
2024-12-01 22:53:09 -0500

    wip matching text

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-    "time"
-    "math/rand"
+	"math/rand"
+	"os"
+	"os/signal"
 	"syscall"
+	"time"
+    "strings"
 )
 
 var data = []string{
@@ -13,26 +16,60 @@ var data = []string{
     "Tis better to have loved and lost than never to have loved at all",
 }
 
+var Green = "\033[32m"
 
 func main() {
     fmt.Println("Starting program...")
 
     rand.Seed(time.Now().UnixNano())
     randIdx := rand.Intn(len(data))
-
     text := data[randIdx]
 
-    fmt.Println(text)
+
+    sigChan := make(chan os.Signal, 1)
+    signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+    go func() {
+        <- sigChan
+        fmt.Println("\nRecevied interrupt signal...")
+        os.Exit(0)
+    }()
+
+
+    // matched := make([]bool, len(text))
+
+    // printText := func(){
+    //     for i, char := range text {
+    //         if matched[i] {
+    //             fmt.Printf("\033[32m%c\033[0m", char)
+    //         } else {
+    //             fmt.Printf("%c", char)
+    //         }
+    //     }
+    //     fmt.Println()
+    // }
+    // printText()
     
+    fmt.Println(text)
+
     for {
         key, err := readKeyStroke()
         if err != nil {
             fmt.Println("Error reading keystroke:", err)
             return
         }
-        fmt.Printf("%c", key)
+        
+        // fmt.Printf("%c", key)
+
+        if strings.Contains(text, string(key)) {
+            fmt.Printf("%s%c%s", Green, key, Green)
+        } else {
+            fmt.Printf("%c", key)
+        }
+
     }
 }
+
 
 func readKeyStroke() (byte, error) {
     var buf [1]byte
